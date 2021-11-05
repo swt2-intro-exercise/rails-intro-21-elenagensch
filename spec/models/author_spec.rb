5f8b052b2f09024f8931168dd7972a3206918a14
genschelena <Elena.Gensch@student.hpi.uni-potsdam.de>
2021-11-05 14:54:05 +0100

    Adds full name

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -10,5 +10,11 @@ RSpec.describe Author, type: :model do
 
   it 'creates author' do
     expect(author.first_name).to eq('Ian')
+    expect(author.last_name).to eq('Turing')
+    expect(author.homepage).to eq('http://wikipedia.de/Alan_Turing')
+  end
+
+  it 'has correct full name' do
+    expect(author.name) .to eq('Ian Turing')
   end
 end
