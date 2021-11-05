0c180f9afbfd64ef3148d9c011babcb0a417125e
genschelena <Elena.Gensch@student.hpi.uni-potsdam.de>
2021-11-05 14:04:20 +0100

    Add author details input fields on new author page

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 describe "New author page", type: :feature do
-  it "shows title" do
+  before do
     visit new_author_path
+  end
+
+  it "shows title" do
     expect(page).to have_text("Create Author")
+  end
+
+  it "should have text inputs for an author's first name, last name, and homepage" do
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
   end
 end
 
