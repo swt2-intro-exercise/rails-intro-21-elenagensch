6875186ad3d84a9a2ceaa76183e0fc90351cd354
genschelena <Elena.Gensch@student.hpi.uni-potsdam.de>
2021-11-08 20:38:54 +0100

    Author's last name not empty

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,20 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+  let(:first_name) { 'Alan' }
+  let(:last_name) { 'Turing' }
+  let(:homepage) { 'http://wikipedia.de/Alan_Turing' }
+
   let(:author) do
     create :author,
-           first_name: 'Ian',
-           last_name: 'Turing',
-           homepage: 'http://wikipedia.de/Alan_Turing'
+           first_name: first_name,
+           last_name: last_name,
+           homepage: homepage
   end
 
-  it 'creates author' do
-    expect(author.first_name).to eq('Ian')
-    expect(author.last_name).to eq('Turing')
-    expect(author.homepage).to eq('http://wikipedia.de/Alan_Turing')
+  it 'should create author' do
+    expect(author.first_name).to eq(first_name)
+    expect(author.last_name).to eq(last_name)
+    expect(author.homepage).to eq(homepage)
   end
 
-  it 'has correct full name' do
-    expect(author.name) .to eq('Ian Turing')
+  it 'should return full name' do
+    expect(author.name).to eq("#{first_name} #{last_name}")
+  end
+
+  it 'should not accept empty last name' do
+    @author = Author.create(first_name: first_name, last_name: '', homepage: homepage)
+    expect(@author).to_not be_valid
   end
 end
